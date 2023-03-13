75ede5b85d55ff0c5e6aec143f7d19478af21621
David Chavez <david@dcvz.io>
2023-03-13 10:55:41 +0100

    Get TTS working

--- examples/cpp/source/main.cpp
+++ examples/cpp/source/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <thread>
 
 #include <helix/helix.h>
 
@@ -7,5 +8,13 @@ auto main() -> int
 {
   auto const message = "Hello, world!";
   std::cout << message << '\n';
+
+  HLXSpeechSynthesizerInit();
+  HLXSpeechSynthesizerSetVolume(1.0);
+  HLXSpeechSynthesizerSpeak("Hello, world!", true);
+
+  // Wait for the speech to finish.
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
   return 0;
 }
