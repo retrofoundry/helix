214350995ae6b5743bfb537cacffb38b7587c13f
David Chavez <david@dcvz.io>
2023-03-14 23:20:11 +0100

    Add Example Project (#6)

---
+++ examples/cpp/README.md
@@ -0,0 +1,7 @@
+# cpp
+
+This is the cpp project example for Helix.
+
+# Building and installing
+
+See the [BUILDING](BUILDING.md) document.
---
+++ examples/cpp/source/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include <helix/speech.h>
+
+auto main() -> int
+{
+  auto const message = "Hello, world!";
+  std::cout << message << '\n';
+
+#if defined(__APPLE__) || defined(__WIN32)
+  HLXSpeechSynthesizerInit();
+  HLXSpeechSynthesizerSetVolume(1.0);
+  HLXSpeechSynthesizerSpeak("Hello, world!", true);
+#endif
+
+  // Wait for the speech to finish.
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+  return 0;
+}
