6b16a866e8cdcaa47fd1ff3e97fe98d30faa949a
David Chavez <david@dcvz.io>
2023-03-14 15:02:04 +0100

    Introduce features

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+#[cfg(feature = "audio")]
 mod audio;
 mod macros;
+#[cfg(feature = "network")]
 mod network;
+#[cfg(feature = "speech")]
 mod speech;
 
 use lazy_static::lazy_static;
@@ -11,16 +14,22 @@ lazy_static! {
 }
 
 pub(crate) struct Helix {
+    #[cfg(feature = "speech")]
     speech_synthesizer: speech::SpeechSynthesizer,
+    #[cfg(feature = "audio")]
     audio_player: audio::AudioPlayer,
+    #[cfg(feature = "network")]
     tcp_stream: network::TCPStream,
 }
 
 impl Helix {
     pub(crate) fn new() -> Helix {
         Helix {
+            #[cfg(feature = "speech")]
             speech_synthesizer: speech::SpeechSynthesizer::new(),
+            #[cfg(feature = "audio")]
             audio_player: audio::AudioPlayer::new(),
+            #[cfg(feature = "network")]
             tcp_stream: network::TCPStream::new(),
         }
     }
