e34d97d481ec1960715c2235b6f25dbcf1d54776
David Chavez <david@dcvz.io>
2023-03-17 12:19:46 +0100

    Use corrosion fork

--- examples/cpp/source/main.cpp
+++ examples/cpp/source/main.cpp
@@ -9,12 +9,10 @@ auto main() -> int
   auto const message = "Hello, world!";
   std::cout << message << '\n';
 
-#if defined(__APPLE__) || defined(__WIN32)
   auto speechSynthesizer = HLXSpeechSynthesizerCreate();
   HLXSpeechSynthesizerInit(speechSynthesizer);
   HLXSpeechSynthesizerSetVolume(speechSynthesizer, 1.0);
   HLXSpeechSynthesizerSpeak(speechSynthesizer, "Hello, world!", true);
-#endif
 
   // Wait for the speech to finish.
   std::this_thread::sleep_for(std::chrono::milliseconds(2000));
