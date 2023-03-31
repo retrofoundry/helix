a24b3e4e46835110525b07552b8cae0ec6aabf4d
David Chavez <david@dcvz.io>
2023-03-31 19:08:00 +0200

    Use SDL2 for window with control over frame

--- examples/rust/src/main.rs
+++ examples/rust/src/main.rs
@@ -1,12 +1,7 @@
 use helix::gui::Gui;
-use std::sync::{Arc, Mutex};
 
 fn main() {
-    let show_app_metrics = Arc::new(Mutex::new(false));
-    let show_app_metrics_clone = Arc::clone(&show_app_metrics);
-
-    let event_loop = Gui::create_event_loop();
-    let gui = Gui::new("Helix Example", &event_loop, |ui| {
+    let mut gui = Gui::new("Helix Example", |ui| {
         ui.menu("File", || {
             ui.menu_item_config("Quit")
                 .shortcut("Ctrl+Q")
@@ -16,18 +11,18 @@ fn main() {
         ui.menu("Edit", || {
 
         });
-    }, move |ui| {
-        let mut show_app_metrics = show_app_metrics_clone.lock().unwrap();
-        ui.show_metrics_window(&mut show_app_metrics);
     }).unwrap();
 
-    let handler = std::thread::spawn(move || {
+    std::thread::spawn(move || {
         loop {
             println!("Hello, world!");
             std::thread::sleep(std::time::Duration::from_millis(1000));
         }
     });
 
-    Gui::start(event_loop, gui);
-    handler.join().unwrap();
+    loop {
+        gui.start_frame();
+        gui.draw_lists();
+        gui.end_frame();
+    }
 }
