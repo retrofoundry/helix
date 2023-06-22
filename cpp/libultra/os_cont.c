e4e31248dc900f17b67ed954efac8a9ceb0c2917
David Chavez <david@dcvz.io>
2023-06-22 16:17:16 +0200

    os_cont: Fix compilation on Windows

--- cpp/libultra/os_cont.c
+++ cpp/libultra/os_cont.c
@@ -1,21 +1,27 @@
 #include <libultra/os_cont.h>
 #include <helix/internal.h>
 
-void *_gamepad_manager;
+// Different name than the reference held in helix.c
+// This is because on Windows the compiler will complain about
+// a redefinition of the variable.
+void *_ref_gamepad_manager;
 
+// Method called by helix.c to setup os_cont with the gamepad manager
 void _osContInternalSetup(void* gamepad_manager) {
-    _gamepad_manager = GamepadManagerCreate();
+    _ref_gamepad_manager = gamepad_manager;
 }
 
+// MARK: - Methods from libultra
+
 s32 osContInit(OSMesgQueue *mq, u8 *controller_bits, OSContStatus *status) {
-    return GamepadManagerInit(_gamepad_manager, controller_bits);
+    return GamepadManagerInit(_ref_gamepad_manager, controller_bits);
 }
 
 s32 osContStartReadData(OSMesgQueue *mesg) {
-    GamepadManagerProcessEvents(_gamepad_manager);
+    GamepadManagerProcessEvents(_ref_gamepad_manager);
     return 0;
 }
 
 void osContGetReadData(OSContPad *pad) {
-    return GamepadManagerGetReadData(_gamepad_manager, pad);
+    return GamepadManagerGetReadData(_ref_gamepad_manager, pad);
 }
